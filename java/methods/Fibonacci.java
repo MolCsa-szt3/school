c586d0179147e45d4fbd7b81b0cdf0b6cd337075
MolCsa-szt3 <155538267+MolCsa-szt3@users.noreply.github.com>
2024-03-26 14:22:33 +0100

    recursion

--- java/methods/Fibonacci.java
+++ java/methods/Fibonacci.java
@@ -12,11 +12,16 @@ public class Fibonacci {
         }
         return nums[1];
     }
+    public static int fibRec(int which){
+        if (which == 0) return 0;
+        if (which < 2)return 1;
+        else return fibRec(which - 2) + fibRec(which - 1);
+    }
     public static void main(String[] args) {
         System.out.print("Hanyadik elemet szeretné tudni? ");
         Scanner scanner = new Scanner(System.in);
         int count = scanner.nextInt();
         scanner.close();
-        System.out.println(fib(count));
+        System.out.println(fibRec(count));
     }
 }
